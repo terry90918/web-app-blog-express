e508554c5489306490837bb0a0079d6b34d1bbee
Terry.TY Chen <zxtw17985321@gmail.com>
2019-01-29 00:53:42 +0800

    刪除文章分類

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -22,10 +22,10 @@ router.get('/categories', (req, res, next) => {
   // info: 伺服器與客戶端溝通暫時儲存資料的地方, 來至於 req.flash('info', '欄位已刪除');
   const message = req.flash('info');
   categoriesRef.once('value').then((snapshot) => {
-    const categories = snapshot.val();    
+    const categories = snapshot.val();
     res.render('dashboard/categories', {
       categories,
-      hasInfo: message.length > 1,
+      hasInfo: message.length > 0,
       message,
       title: 'Categories',
     });
@@ -35,20 +35,43 @@ router.get('/categories', (req, res, next) => {
 /* 新增文章分類 */
 router.post('/categories/create', (req, res, next) => {
   const data = req.body;
+  console.log('data', data)
   const categoryRef = categoriesRef.push();
   const key = categoryRef.key;
   data.id = key;
-  categoryRef
-    .set(data)
-    .then(() => {
-      res.redirect('/dashboard/categories')
-    })
+  categoriesRef
+    .orderByChild('name')
+    .equalTo(data.name)
+    .once('value')
+    .then((snapshot) => {
+      if (snapshot.val() !== null) {
+        req.flash('info', '已有相同分類名稱');
+        res.redirect('/dashboard/categories');
+      } else {
+        categoriesRef
+          .orderByChild('path')
+          .equalTo(data.path)
+          .once('value')
+          .then((snapshot) => {
+            if (snapshot.val() !== null) {
+              req.flash('info', '已有相同路徑');
+              res.redirect('/dashboard/categories');
+            } else {
+              categoryRef
+                .set(data)
+                .then(() => {
+                  res.redirect('/dashboard/categories');
+                });
+            }
+          });
+      }
+    });
 });
 
 /* 刪除文章分類 */
 router.post('/categories/delete/:id', (req, res, next) => {
   const id = req.param('id');
-  categoriesRef.child(id).remove;
+  categoriesRef.child(id).remove();
   req.flash('info', '欄位已刪除');
   res.redirect('/dashboard/categories');
 });
