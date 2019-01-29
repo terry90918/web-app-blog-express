82fbda8bc49031b2f45f7668c64f8ff5cfc31e12
TerryChen <zxtw17985321@gmail.com>
2019-01-29 18:11:20 +0800

    更新

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,20 +1,121 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../connections/firebase_admin');
+const striptags = require('striptags');
+const moment = require('moment');
 
-const categoriesRef = db.ref('/blog/categories/')
+const categoriesRef = db.ref('/blog/categories/');
+const articlesRef = db.ref('/blog/articles/');
 
 // const ref = db.ref('any');
 // ref.once('value', (snapshot) => {
 //   console.log('/dashboard', snapshot.val());
 // });
 
+/* 取得文章列表 */
 router.get('/archives', (req, res, next) => {
-  res.render('dashboard/archives', { title: 'Archives' });
+  const status = req.query.status || 'public';
+  let categories = {};
+  let articles = [];
+  categoriesRef
+    .once('value')
+    .then((snapshot) => {
+      categories = snapshot.val();
+      return articlesRef
+        .orderByChild('update_time')
+        .once('value');
+    })
+    .then((snapshot) => {
+      snapshot.forEach((snapshotChild) => {
+        const child = snapshotChild.val();
+        if (status === child.status) {
+          articles.push(child);
+        }
+      });
+      return Promise.resolve(articles.reverse());
+    })
+    .then(() => {
+      res.render('dashboard/archives', {
+        articles,
+        categories,
+        moment,
+        status,
+        striptags,
+        title: 'Archives',
+      });
+    });
 });
 
-router.get('/article', (req, res, next) => {
-  res.render('dashboard/article', { title: 'Article' });
+/* 取得新增文章的需要資訊 */
+router.get('/article/create', (req, res, next) => {
+  categoriesRef
+    .once('value')
+    .then((snapshot) => {
+      const categories = snapshot.val();
+      res.render('dashboard/article', {
+        categories,
+        title: 'Article Create'
+      });
+    });
+});
+
+/* 取得更新文章的需要資訊 */
+router.get('/article/:id', (req, res, next) => {
+  const id = req.params.id;
+  let categories = {};
+  let article = {};
+  categoriesRef
+    .once('value')
+    .then((snapshot) => {
+      categories = snapshot.val();
+      return articlesRef
+        .child(id)
+        .once('value');
+    })
+    .then((snapshot) => {
+      article = snapshot.val();
+      res.render('dashboard/article', {
+        article,
+        categories,
+        title: 'Article: ' + id
+      });
+    });
+});
+
+/* 新增文章 */
+router.post('/article/create', (req, res, next) => {
+  const data = req.body;
+  const articleRef = articlesRef.push();
+  const key = articleRef.key;
+  const updateTime = Math.floor(Date.now() / 1000);
+  data.id = key;
+  data.update_time = updateTime;
+  articleRef
+    .set(data)
+    .then(() => {
+      res.redirect(`/dashboard/article/${key}`);
+    });
+});
+
+/* 更新文章 */
+router.post('/article/update/:id', (req, res, next) => {
+  const data = req.body;
+  const id = req.params.id;
+  articlesRef
+    .child(id)
+    .update(data)
+    .then(() => {
+      res.redirect(`/dashboard/article/${id}`);
+    });
+});
+
+/* 刪除文章 */
+router.post('/article/delete/:id', (req, res, next) => {
+  const id = req.params.id;
+  articlesRef.child(id).remove();
+  req.flash('info', '文章已刪除');
+  res.send('文章已刪除');
+  res.end();
 });
 
 /* 取得文章分類 */
@@ -35,7 +136,6 @@ router.get('/categories', (req, res, next) => {
 /* 新增文章分類 */
 router.post('/categories/create', (req, res, next) => {
   const data = req.body;
-  console.log('data', data)
   const categoryRef = categoriesRef.push();
   const key = categoryRef.key;
   data.id = key;
@@ -48,29 +148,35 @@ router.post('/categories/create', (req, res, next) => {
         req.flash('info', '已有相同分類名稱');
         res.redirect('/dashboard/categories');
       } else {
-        categoriesRef
-          .orderByChild('path')
-          .equalTo(data.path)
-          .once('value')
-          .then((snapshot) => {
-            if (snapshot.val() !== null) {
-              req.flash('info', '已有相同路徑');
-              res.redirect('/dashboard/categories');
-            } else {
-              categoryRef
-                .set(data)
-                .then(() => {
-                  res.redirect('/dashboard/categories');
-                });
-            }
-          });
+        return Promise.resolve('Success');
       }
+    })
+    .then(() => {
+      return categoriesRef
+        .orderByChild('path')
+        .equalTo(data.path)
+        .once('value');
+    })
+    .then((snapshot) => {
+      if (snapshot.val() !== null) {
+        req.flash('info', '已有相同路徑');
+        res.redirect('/dashboard/categories');
+      } else {
+        return Promise.resolve('Success');
+      }
+    })
+    .then(() => {
+      categoryRef
+        .set(data)
+        .then(() => {
+          res.redirect('/dashboard/categories');
+        });
     });
 });
 
 /* 刪除文章分類 */
 router.post('/categories/delete/:id', (req, res, next) => {
-  const id = req.param('id');
+  const id = req.params.id;
   categoriesRef.child(id).remove();
   req.flash('info', '欄位已刪除');
   res.redirect('/dashboard/categories');
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../connections/firebase_admin');
+const striptags = require('striptags');
+const moment = require('moment');
+
+const categoriesRef = db.ref('/blog/categories/');
+const articlesRef = db.ref('/blog/articles/');
 
 // const ref = db.ref('any');
 // ref.once('value', (snapshot) => {
 //   console.log('/', snapshot.val());
 // });
 
+/* 取得文章列表 */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  let categories = {};
+  let articles = [];
+  categoriesRef
+    .once('value')
+    .then((snapshot) => {
+      categories = snapshot.val();
+      return articlesRef
+        .orderByChild('update_time')
+        .once('value');
+    })
+    .then((snapshot) => {
+      snapshot.forEach((snapshotChild) => {
+        const child = snapshotChild.val();
+        if ('public' === child.status) {
+          articles.push(child);
+        }
+      });
+      return Promise.resolve(articles.reverse());
+    })
+    .then(() => {
+      res.render('index', {
+        articles,
+        categories,
+        moment,
+        striptags,
+        title: 'Express',
+      });
+    });
 });
 
 router.get('/post', (req, res, next) => {
